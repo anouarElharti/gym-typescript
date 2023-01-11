f6d7e8508596cc336490755e481e0a5bf8b3e446
Elharti <AElharti@lims.com>
2023-01-11 17:02:46 +0100

    Adding the Benefits page

--- src/scenes/Benefits/index.tsx
+++ src/scenes/Benefits/index.tsx
@@ -1,16 +1,158 @@
-import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HText from "@/shared/HText";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import {
   HomeModernIcon,
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "Go the extra mile",
+    description:
+      "I don't count my sit-ups. I only start counting when it starts hurting because they're the only ones that count.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "It’s never crowded",
+    description:
+      "I don't count my sit-ups. I only start counting when it starts hurting because they're the only ones that count.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Take care of your body",
+    description:
+      "I don't count my sit-ups. I only start counting when it starts hurting because they're the only ones that count.",
+  },
+];
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Benefits = ({ setSelectedPage }: Props) => {
-  return <section id="benefits" className="mx-auto min-h-full"></section>;
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* HEADER */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="md:my-5 md:w-3/5"
+        >
+          <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
+            “Never give up on a dream just because of the time it will take to
+            accomplish it. <br />
+            The time will pass anyway.”
+          </p>
+        </motion.div>
+        {/* BENEFITS */}
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+        {/* GRAPHICS AND DESCRIPTION */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* GRAPHIC */}
+          <img
+            alt="Benefits page graphic"
+            src={BenefitsPageGraphic}
+            className="mx-auto"
+          />
+          {/* DESCRIPTION */}
+          <div className="">
+            {/* TITLE */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    You did not wake up today to be{" "}
+                    <span className="text-primary-500">mediocre</span>.
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+            {/* DESCRIPTION */}
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Your health account, your bank account, they're the same thing.
+                The more you put in, the more you can take out.
+                <br />
+                There are two types of pain in this world: pain that hurts you,
+                and pain that changes you.
+              </p>
+              <p className="mb-5">
+                Ability is what you're capable of doing. Motivation determines
+                what you do. Attitude determines how well you do it.
+                <br />I hated every minute of training, but I said, Don't quit.
+                Suffer now and live the rest of your life as a champion.
+              </p>
+            </motion.div>
+            {/* BUTTON */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Benefits;
